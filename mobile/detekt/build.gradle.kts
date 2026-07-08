4ab0d18745f776f50fcece1c5d948001ea44cab6
Matej Drobnič <matej@matejdro.com>
2026-07-08 09:53:14 +0200

    test(detekt): fix detekt tests failing

--- mobile/detekt/build.gradle.kts
+++ mobile/detekt/build.gradle.kts
@@ -20,10 +20,33 @@ dependencyAnalysis {
    }
 }
 
+// Workaround from https://github.com/lunaynx/SkyHanni/commit/2acc2ffe6909f6bbfb9ebb0b383dff1e774d248a
+abstract class DetektTestMetadataRule : ComponentMetadataRule {
+   override fun execute(context: ComponentMetadataContext) {
+      val version = context.details.id.version
+      if (version != "2.0.0-alhpa.4" && version != "2.0.0-alpha.5") return
+
+      context.details.withVariant("runtimeElements") {
+         withDependencies {
+            // detekt-test 2.0.0-alpha.4 and 2.0.0-alpha.5 request detekt-api test fixtures, but
+            // detekt-api only publishes fixture sources.
+            removeAll { it.group == "dev.detekt" && it.name == "detekt-api" }
+            add("dev.detekt:detekt-api:$version")
+         }
+      }
+   }
+}
+
 
 dependencies {
    compileOnly(libs.detekt.api)
 
    testImplementation(libs.detekt.api)
    testImplementation(libs.detekt.test)
+
+   // TODO remove this once Detekt 2.0.0-alpha.6 is out
+   // https://github.com/detekt/detekt/issues/9409
+   components {
+      withModule<DetektTestMetadataRule>("dev.detekt:detekt-test")
+   }
 }
